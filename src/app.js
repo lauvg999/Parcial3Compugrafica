f563bc17ff14cf057a76a5e965453895086562e9
lauvg999 <lauvg999@gmail.com>
2022-11-24 14:55:13 -0500

    Ultimos cambios

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.131.3/examples/jsm/l
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  40,
+  50,
   window.innerWidth / window.innerHeight,
   1,
   5000,
@@ -14,17 +14,14 @@ let loader = new GLTFLoader();
 
 const textureLoader = new THREE.TextureLoader();
 let map = textureLoader.load(
-  /* 'assets/modelFour/textures/Material.010_baseColor.png', */
   'assets/modelOne/textures/Material_baseColor.png'
 );
 
 let map1 = textureLoader.load(
-  /* 'assets/modelThree/textures/coat_baseColor.png', */
   'assets/texturas/1.jpg'
 );
 
 let map2 = textureLoader.load(
-  /* 'assets/modelThree/textures/glass_baseColor.png', */
   'assets/texturas/2.jpg'
 );
 
@@ -38,7 +35,6 @@ map2.encoding = THREE.sRGBEncoding;
 map2.flipY = false;
 
 let car;
-let hlight;
 let directionalLight;
 let light;
 let light2;
@@ -128,7 +124,6 @@ export function changeTexture() {
       node.material.map = map;
     }
   });
-  //car.material = new THREE.MeshPhongMaterial({ map: map, color: 0xff00ff });
   animate();
 }
 
@@ -138,7 +133,6 @@ export function changeTexture1() {
       node.material.map = map1;
     }
   });
-  //car.material = new THREE.MeshPhongMaterial({ map: map, color: 0xff00ff });
   animate();
 }
 
@@ -148,7 +142,6 @@ export function changeTexture2() {
       node.material.map = map2;
     }
   });
-  //car.material = new THREE.MeshPhongMaterial({ map: map, color: 0xff00ff });
   animate();
 }
 
@@ -165,14 +158,6 @@ function cameraConfig() {
 }
 
 function setupLights() {
-  /* hlight = new THREE.AmbientLight(0x404040, 100);
-  scene.add(hlight);
-
-  directionalLight = new THREE.DirectionalLight(0xffffff, 50);
-  directionalLight.position.set(0, 1, 0);
-  directionalLight.castShadow = true;
-  scene.add(directionalLight);
-   */
   directionalLight = new THREE.DirectionalLight(0xffffff, 20);
   directionalLight.position.set(0, 1, 0);
   scene.add(directionalLight);
